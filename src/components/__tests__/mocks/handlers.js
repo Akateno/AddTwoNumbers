217d4d88f0655a5727173b4caad7ec65553de0fc
Erick Vargas <erickkvargas@gmail.com>
2022-08-05 11:00:27 -0700

    added procfile

--- src/components/__tests__/mocks/handlers.js
+++ src/components/__tests__/mocks/handlers.js
@@ -4,10 +4,10 @@ import { rest } from "msw";
 let numbers = data;
 
 export const handlers = [
-  rest.get("http://localhost:4000/numbers", (req, res, ctx) => {
+  rest.get("http://localhost:60001/numbers", (req, res, ctx) => {
     return res(ctx.json(numbers));
   }),
-  rest.post("http://localhost:4000/numbers", (req, res, ctx) => {
+  rest.post("http://localhost:60001/numbers", (req, res, ctx) => {
     const id = numbers[numbers.length - 1]?.id + 1 || 1;
     const number = { id, ...req.body };
     numbers.push(number);
